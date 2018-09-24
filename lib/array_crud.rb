d45cc4baa75e05b74f590ff59331572602f86e8d
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-24 14:51:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  elements = [1,2,3,4]
+  array = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-  elements.push(5)
+  array.push(5)
 end
 
 def add_element_to_start_of_array(array, element)
